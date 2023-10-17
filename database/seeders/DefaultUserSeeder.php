7ff9a803e2b7a0c215002b0ef73cef5b335b73f8
CostaMateus <costa.mack95@gmail.com>
2023-10-16 22:08:48 -0300

    fix: change default user

--- database/seeders/DefaultUserSeeder.php
+++ database/seeders/DefaultUserSeeder.php
@@ -15,11 +15,11 @@ class DefaultUserSeeder extends Seeder
      */
     public function run()
     {
-        if (User::where('email', 'john.doe@helper.app')->count() == 0) {
+        if (User::where('email', 'mateus@costamateus.com.br')->count() == 0) {
             $user = User::create([
-                'name' => 'John DOE',
-                'email' => 'john.doe@helper.app',
-                'password' => bcrypt('Passw@rd'),
+                'name' => 'Mateus Costa',
+                'email' => 'mateus@costamateus.com.br',
+                'password' => bcrypt('Costa6151!'),
                 'email_verified_at' => now()
             ]);
             $user->creation_token = null;
